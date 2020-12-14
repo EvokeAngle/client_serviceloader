2a09fd08545aa02ea8031fef6ed73ba76365ba1b
Luo Jian <18403559387@163.com>
2020-12-14 17:02:13 +0800

    add json

--- service-consumer/src/main/java/module-info.java
+++ service-consumer/src/main/java/module-info.java
@@ -4,8 +4,12 @@ module serviceconsumer {
 	requires serviceinterface;
     requires javafx.graphics;
     requires javafx.controls;
+    requires fastjson;
+    requires java.sql;
+    opens bean;
 
     uses DisneyWorldMember;
+
 
     exports serviceconsumer;
 
